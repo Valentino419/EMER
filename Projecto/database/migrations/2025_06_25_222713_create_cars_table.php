7abd118fc31f0600f4c68dafd90d4df52fc4e2f9
camilaSittner <164429595+camsittner@users.noreply.github.com>
2025-08-04 18:32:41 -0300

    Revert "Camii"

--- Projecto/database/migrations/2025_06_25_222713_create_cars_table.php
+++ Projecto/database/migrations/2025_06_25_222713_create_cars_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('car_plate');
+           
             $table->timestamps();
         });
     }
--- Projecto/resources/js/app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-function App() {
-    return <h1>¡Hola desde React dentro de Laravel! 🚀</h1>;
-}
-
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
