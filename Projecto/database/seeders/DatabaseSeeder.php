9321c69c51f40822b7c436c669f6c91819119a40
Yanis155 <yanis.sco@gmail.com>
2025-08-05 20:06:27 -0300

    creacion Crud zones

--- Projecto/database/seeders/DatabaseSeeder.php
+++ Projecto/database/seeders/DatabaseSeeder.php
@@ -2,10 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
- use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+//use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Database\Seeders\ZoneSeeder;
+use App\Models\User;
+use App\Models\Zone;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,11 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-         User::factory(10)->create();
+        // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call(ZoneSeeder::class);
     }
 }
