2695ec89d48656de8d747d82dbebe988a560c633
Yanis155 <yanis.sco@gmail.com>
2025-08-07 18:38:59 -0300

    Creacion de ParkingSession y logincontroller

--- Projecto/routes/web.php
+++ Projecto/routes/web.php
@@ -5,18 +5,23 @@ use Inertia\Inertia;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CarController;
 use App\Http\Controllers\ZoneController;
-
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\ParkingSessionController;
 Route::resource('cars', CarController::class);
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::get('dashboard', function () {
+//         return Inertia::render('dashboard');
+//     })->name('dashboard');
+// });
 
 Route::resource('users', UserController::class)->names([
     'index' => 'user.index',
@@ -35,6 +40,9 @@ Route::resource('zones', ZoneController::class)->names([
     'update' => 'zone.update',
     'destroy' => 'zone.destroy',
 ]);
+
+Route::get('/parking/create', [ParkingSessionController::class, 'create'])->name('parking.create');
+Route::post('/parking', [ParkingSessionController::class, 'store'])->name('parking.store');
 
 
 Route::fallback(function () {
