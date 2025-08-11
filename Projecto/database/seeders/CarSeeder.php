9af01e17f250a51fb71b726850959ac08d4abceb
Yanis155 <yanis.sco@gmail.com>
2025-08-11 16:49:06 -0300

    login casi terminado

--- Projecto/database/seeders/CarSeeder.php
+++ Projecto/database/seeders/CarSeeder.php
@@ -4,7 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\Car;
 class CarSeeder extends Seeder
 {
     /**
@@ -12,6 +13,18 @@ class CarSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('cars')->insert([
+            'user_id'=>'5',
+            'car_plate'=>'ARF',
+        ]);
         
+        DB::table('cars')->insert([
+            'user_id'=>'2',
+            'car_plate'=>'BCA',
+        ]);
+        DB::table('cars')->insert([
+            'user_id'=>'3',
+            'car_plate'=>'ABC',
+        ]);
     }
 }
